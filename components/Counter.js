c334969ebe596af2b40df93f4be9b9a5956d68b1
Ahkal <bp@sojourn.llc>
2022-08-23 10:55:28 -0400

    foundation

---
+++ components/Counter.js
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+
+const Counter = ({ title, description }) => {
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+
+  useEffect(() => {
+    console.log("Use effect ran.");
+    console.log(count);
+  });
+
+  return (
+    <>
+      <h1 className="title text-4xl">{title}</h1>
+      <aside>{description}</aside>
+      <p className="mt-4 mb-4 text-2xl italic">{count}</p>
+      <button
+        type="button"
+        onClick={handleClick}
+        className="focus:outline-none text-black bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm mt-4 px-5 py-2.5 mr-2 mb-2"
+      >
+        Add 1
+      </button>
+    </>
+  );
+};
+
+export default Counter;
